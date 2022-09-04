8e341eda293f72d0b58c10027a6fd21f82266dc6
Константин <eckumoc@gmail.com>
2022-09-04 06:17:08 +0300

    ╨б╨╛╨╖╨┤╨░╤О ╨╜╨╛╨▓╤Л╨╣ ╨┐╤А╨╛╨╡╨║╤В

---
+++ CsCompilerModule/CompileApplicationProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+ 
+public class CompileApplicationProgram
+{
+
+    public static void Run(string[] args)
+    {          
+        var program = new CompileApplicationSources(@"D:\app1");
+        program.AddClass(
+            "WwwClient", 
+           @"public class ProgramTest
+            {
+                public static void Main(){
+                    System.Console.WriteLine(""this is a successfull compiled program"");
+                }
+            }
+        "); 
+        program.Run();
+    }
+}
+ 
---
+++ bin/Debug/net5.0/WwwClient.cs
@@ -0,0 +1,8 @@
+            
+            public class ProgramTest
+            {
+                public static void Main(){
+                    System.Console.WriteLine("this is a successfull compiled program");
+                }
+            }
+        
